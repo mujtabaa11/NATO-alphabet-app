64fff72410021f60a79b7990097bd86adea53e3c
Mohamed Mujtaba <Mohamed.Mugtaba11@gmail.com>
2024-07-17 03:04:03 -0400

    Nato-alphabet_app

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas
+
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+    
+
+new_word = input("Enter a new word: ").upper()
+result = [new_dict[letter] for letter in new_word]
+print (result)
